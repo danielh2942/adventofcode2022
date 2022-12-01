903c3e90304d148ecfb5c50830bcdcdebc172d75
Daniel Hannon <hello111f@gmail.com>
2022-12-01 10:01:45 +0000

    Tidied up solution

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -16,33 +16,23 @@ int main() {
 	}
 
 	std::string temp;
-	int max = 0;
 	int tally = 0;
-	int numElves = 1;
 	while(std::getline(inp, temp)) {
 		if(temp == "") {
 			tallies.push_back(tally);
-			if(tally > max) {
-				max = tally;
-			}
 			tally = 0;
-			numElves++;
 			continue;
 		}
 		tally += std::atoi(temp.data());
 	}
 
-	numElves++;
 	tallies.push_back(tally);
-	if (tally > max) {
-		max = tally;
-	}
 
 	inp.close();
 
-	std::cout << "Part 1 Most weight carried by an elf: " << max << '\n';
-
 	std::sort(tallies.begin(),tallies.end(), std::greater<int>());
+
+	std::cout << "Part 1: Most weight carried by an elf: " << tallies[0] << '\n';
 
 	std::cout << "Part 2: Total calories carried by the top three elves: " << (tallies[0] + tallies[1] + tallies[2]) << '\n';
 	return 0;
